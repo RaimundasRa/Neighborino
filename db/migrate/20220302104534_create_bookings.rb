00b6a6c1ddf31b1f6596157d7f04c035ed6cfe6c
Raimundas Radavicius <raimundas_radavicius@yahoo.com>
2022-03-02 11:40:04 +0000

    bookings done

---
+++ db/migrate/20220302104534_create_bookings.rb
@@ -0,0 +1,12 @@
+class CreateBookings < ActiveRecord::Migration[6.1]
+  def change
+    create_table :bookings do |t|
+      t.integer :start
+      t.integer :end
+      t.references :user, null: false, foreign_key: true
+      t.references :activity, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
