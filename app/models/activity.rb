73bd10afe0bcee9d7d5507bf0b9c68af87cd5283
Fiorella Salazar <sajifio@gmail.com>
2022-03-03 13:07:57 +0000

    Add form to create new activity with multiple images

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -4,4 +4,5 @@ class Activity < ApplicationRecord
   has_many :bookings, dependent: :destroy
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+  has_many_attached :image, dependent: :destroy
 end
