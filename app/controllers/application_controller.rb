932cac86de32cd77eb7a32d370ca0f793bfeadba
Fiorella Salazar <sajifio@gmail.com>
2022-03-04 16:14:29 +0000

    Add booking logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,5 +10,5 @@ class ApplicationController < ActionController::Base
 
   # For additional in app/views/devise/registrations/edit.html.erb
   devise_parameter_sanitizer.permit(:account_update, keys: [:username, :email, :password, :photo, :area_id])
-end
+  end
 end
