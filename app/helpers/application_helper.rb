efb7055a581268c434e196e3d4fe9d2e2b8eca39
DeanTResi <dean@resi.co.uk>
2022-03-04 14:39:05 +0000

    londontown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,29 @@ module ApplicationHelper
   def areas_list
     Area.all
   end
+
+  def area_image_url
+    images = ['https://res.cloudinary.com/dqat8a2hi/image/upload/v1646403248/neighbourino%20%28Neighborino%29-production/Area000.jpg',
+              'https://res.cloudinary.com/dqat8a2hi/image/upload/v1646403246/neighbourino%20%28Neighborino%29-production/Area003.jpg',
+              'https://res.cloudinary.com/dqat8a2hi/image/upload/v1646403245/neighbourino%20%28Neighborino%29-production/Area005.jpg',
+              'https://res.cloudinary.com/dqat8a2hi/image/upload/v1646403245/neighbourino%20%28Neighborino%29-production/Area006.jpg',
+              'https://res.cloudinary.com/dqat8a2hi/image/upload/v1646403244/neighbourino%20%28Neighborino%29-production/Area007.jpg',
+              'https://res.cloudinary.com/dqat8a2hi/image/upload/v1646403244/neighbourino%20%28Neighborino%29-production/Area008.jpg',
+              'https://res.cloudinary.com/dqat8a2hi/image/upload/v1646403243/neighbourino%20%28Neighborino%29-production/Area010.jpg',
+              'https://res.cloudinary.com/dqat8a2hi/image/upload/v1646403244/neighbourino%20%28Neighborino%29-production/Area011.jpg',
+              'https://res.cloudinary.com/dqat8a2hi/image/upload/v1646403241/neighbourino%20%28Neighborino%29-production/Area012.jpg',
+              'https://res.cloudinary.com/dqat8a2hi/image/upload/v1646403241/neighbourino%20%28Neighborino%29-production/Area013.jpg',
+              'https://res.cloudinary.com/dqat8a2hi/image/upload/v1646403241/neighbourino%20%28Neighborino%29-production/Area014.jpg',
+              'https://res.cloudinary.com/dqat8a2hi/image/upload/v1646403241/neighbourino%20%28Neighborino%29-production/Area015.jpg',
+              'https://res.cloudinary.com/dqat8a2hi/image/upload/v1646403240/neighbourino%20%28Neighborino%29-production/Area020.jpg',
+              'https://res.cloudinary.com/dqat8a2hi/image/upload/v1646403238/neighbourino%20%28Neighborino%29-production/Area021.jpg',
+              'https://res.cloudinary.com/dqat8a2hi/image/upload/v1646403238/neighbourino%20%28Neighborino%29-production/Area023.jpg',
+              'https://res.cloudinary.com/dqat8a2hi/image/upload/v1646403237/neighbourino%20%28Neighborino%29-production/Area025.jpg',
+              'https://res.cloudinary.com/dqat8a2hi/image/upload/v1646403237/neighbourino%20%28Neighborino%29-production/Area026.jpg']
+
+    images.sample
+  end
+
+  def category_image
+  end
 end
